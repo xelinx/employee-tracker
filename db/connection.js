d1e6b6fc80af75b952741803ec8c954797c4cdb7
Fog <lin.emay@gmail.com>
2020-09-13 02:56:16 -0700

    combine prompts to server, fix connection

--- db/connection.js
+++ db/connection.js
@@ -8,15 +8,13 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "Meadows2020!@#",
     database: "kdc_db"
 });
 
 connection.connect(function(err){
     if (err) throw err;
     console.log ("Connection Successful")
-    //run prompts after connection is made
-    prompts();
 });
 
 connection.query = util.promisify(connection.query);
--- server.js
+++ server.js
@@ -3,19 +3,65 @@
 //Dependencies
 const inquirer = require("inquirer");
 const logo = require("asciiart-logo");
-const prompts = require("./prompts");
 const db = require("./db");
-//require("console.table");
+require("console-table");
 
 require("./db/connection")
 
+prompts();
 
-async function viewAllTitles() {
-
-    const titles = await db.viewAllTitles();
-
-    console.log("\n");
-    console.table(titles);
-
-    mainPrompt();
-}
+function prompts() {
+    inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "MAIN MENU",
+      choices: [
+        "View all employees",
+        "View all employees by department",
+        "View all employees by manager",
+        "View all employees by role",
+        "Add employee",
+        "Add role",
+        "Add department",
+        "Update employee role",
+        //"Update employee manager",
+        //"Delete department",
+        //"Delete role",
+        //"Delete employee",
+        //"View department budgets",
+        "Exit"
+      ]
+    }).then(function(response) {
+            switch (response.action) {
+                case "View All Employees":
+                    viewAllEmployee();
+                    break;
+                case "View All Employees By Department":
+                    viewAllEmployeeByDep();
+                    break;
+                case "View All Employees By Manager":
+                    viewAllEmployeeByManager();
+                    break;
+                case "View All Employees By Role":
+                    viewAllEmployeeByRole();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;   
+                case "Add Department":
+                    addDepartment();
+                    break;                                        
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                case "Exit":
+                    process.exit();
+                default:
+                    break;
+            }
+        })                    
+}        
